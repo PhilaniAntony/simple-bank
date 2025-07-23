c9ba1a19ee854b576e6dc67d5fa408b11837ca94
Philani Antony Dlamini <philaniantonydlamini@gmail.com>
2025-07-23 22:01:23 +0200

    Load environment variable before running db migrations

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ echo "Waiting for Postgres..."
 /app/wait-for.sh postgres:5432 -- echo "Postgres is up"
 
 echo "Running DB migrations..."
+source /app/app.env
 /app/migrate -path /app/db/migration -database "$DB_SOURCE" -verbose up
 
 echo "Starting API server..."
